f7598c2b15caaa0ea87852b27f37e7c0e7f4513c
rakeshmenasi <rakeshmenasi903@gmail.com>
2024-04-29 15:23:19 +0530

    first commit

---
+++ src/components/item/item.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+const Item = ({ data, addToCart }) => {
+
+    const { id, image, title, price } = data
+
+    return (
+        <div className="card">
+            <div className="grid">
+                <div className="image">
+                    <img src={image} alt="" />
+                </div>
+                <div className="title">
+                    <Link to={`/product/${id}`} className="link titleLink">
+                        {title}
+                    </Link>
+                </div>
+                <div className="flex">
+                    <span className="price" style={{ marginRight: 15 }}>
+                        ${price}
+                    </span>
+                    <div className="cart" onClick={addToCart}>
+                        <img title="Add to cart" className="cartImg" src="/add.svg" alt="" />
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { Item }
